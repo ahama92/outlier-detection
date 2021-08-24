a3bb9eba5629d53139689fdfde0c78295b2f8cbf
Ahama1992 <mohammad.91225@gmail.com>
2021-08-24 01:56:01 -0700

    Performing tests

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ from sklearn.ensemble import IsolationForest
 from sklearn.svm import OneClassSVM
 from sklearn.cluster import DBSCAN
     
-def density_based_spatial_clustering(eps=0.25,min_samples=10):
-    outlier_detection = DBSCAN(eps=eps,min_samples=min_samples)
-    df['density_based_spatial_clustering']=outlier_detection.fit_predict(data)
-    anomaly=df.loc[df['density_based_spatial_clustering']==-1 , ['residual']]
-    anomaly=anomaly.loc[anomaly['residual']>df['residual'].mean()]
-    
-    return anomaly
-    
 def support_vector_machine_based(threshold=0.005):
     model = OneClassSVM(nu=0.005, kernel="rbf", gamma=threshold)
     model.fit(data)
@@ -23,11 +15,19 @@ def support_vector_machine_based(threshold=0.005):
     
     return anomaly
 
-def isolation_forest(threshold=0.002):
+def isolation_forest(threshold=0.001):
     model =  IsolationForest(contamination=threshold)
     model.fit(data) 
     df['isolation_forest'] = pandas.Series(model.predict(data))
     anomaly=df.loc[df['isolation_forest']==-1 , ['residual']]
+    anomaly=anomaly.loc[anomaly['residual']>df['residual'].mean()]
+    
+    return anomaly
+    
+def density_based_spatial_clustering(eps=0.25,min_samples=10):
+    outlier_detection = DBSCAN(eps=eps,min_samples=min_samples)
+    df['density_based_spatial_clustering']=outlier_detection.fit_predict(data)
+    anomaly=df.loc[df['density_based_spatial_clustering']==-1 , ['residual']]
     anomaly=anomaly.loc[anomaly['residual']>df['residual'].mean()]
     
     return anomaly
@@ -40,14 +40,14 @@ def main():
         ax.plot(df.index,df['residual'], color='blue', label = 'Normal')
         ax.scatter(anomaly.index,anomaly['residual'], color='red', label = 'Anomaly')
 
-    kwargs = dict(y=0.95, x=0.05, ha='left', va='top')
+    kwargs = dict(y=0.95, x=0.15, ha='left', va='top')
     for j in range(len(func_list)):
         axes[j].set_title(func_list[j].__name__, **kwargs)
         fig.suptitle('Iteration = {}'.format(i+1), size=14)
 
 data=[]
-for i in range(1,10):
-    df=pandas.read_csv('./data/burgers-test-1/res'+str(i)+'.csv',skiprows=2,header=None)
+for i in range(0,50):
+    df=pandas.read_csv('./data/burgers-test-4/res'+str(i)+'.csv',skiprows=2,header=None)
     df.rename(columns={list(df)[0]:'residual'},inplace=True)
     df=df.abs()
     df[df<1e-20]=1e-20
